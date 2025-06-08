ce61204c08929723750bdcc68c5ff9ab5854d12f
play <juninhomacedo2010@hotmail.com>
2025-06-08 10:18:27 -0300

    Ajuste para criar gerar novo token

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,6 +76,8 @@ export default function App() {
     } catch (erro) {
       console.error(`Erro ao executar chamada para exibir produtos da grid -> ${erro}`);
 
+      localStorage.removeItem('token');
+      
       gerarToken().then(token => {carregarGrid(pagina, limite, token ?? '')});
 
       setError('Erro ao carregar produtos. Tente novamente mais tarde.');
